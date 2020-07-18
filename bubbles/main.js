52095d0005c34ec6396977ad1230583b21dcfd63
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2020-07-17 17:35:32 -0700

    Update main.js

--- bubbles/main.js
+++ bubbles/main.js
@@ -6,6 +6,7 @@ function bubble(x, y, speed) {
   this.size = speed * 5;
   this.move = function() {
     this.y += this.speed;
+    this.speed++;
   }
   this.display = function() {
     fill(0, 60, 255);
